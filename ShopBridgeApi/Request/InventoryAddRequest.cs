58d603701eb139c1a1bf6702cd03bc414e56ae23
Pradeep Prajapati <prdpprajapati07@gmail.com>
2021-05-28 15:02:36 +0530

    Add files via upload

---
+++ ShopBridgeApi/Request/InventoryAddRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopBridgeApi.Request
+{
+    public class InventoryAddRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ ShopBridgeApi/Request/InventoryUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopBridgeApi.Request
+{
+    public class InventoryUpdateRequest
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
