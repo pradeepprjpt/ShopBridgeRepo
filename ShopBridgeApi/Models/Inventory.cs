b29c357e7b42ff1b2e491f0d84611c894e01cec2
Pradeep Prajapati <prdpprajapati07@gmail.com>
2021-05-28 15:01:30 +0530

    Add files via upload

---
+++ ShopBridgeApi/Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopBridgeApi.Models
+{
+    public class Inventory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
